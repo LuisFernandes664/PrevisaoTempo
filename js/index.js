bc97f7a632e121593c534dfb3c9670b95db6d140
LuisFernandes664 <82168814+LuisFernandes664@users.noreply.github.com>
2022-07-13 00:27:39 +0100

    header + img ( HTML & CSS )

--- js/index.js
+++ js/index.js
@@ -2,21 +2,35 @@
 
 const apikey = '0fe15371e2f5ef4035accd68fb8832ac';
 
-let city = document.getElementById('search').value;
-console.log(city)
+const form = document.getElementById('form')
+form.addEventListener('submit', getCity);
 
-    const form = document.getElementById('form')
-    form.addEventListener('submit', getCity(city, apikey));
-
-function getCity(city, apikey){
-    city.preventDefault()
+function getCity(e){
+    //nao fazer load à pagina quando faço submit
+    e.preventDefault();
+    let city = document.getElementById('search').value;
     console.log(city);
     console.log(apikey);
     return fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apikey}`)
             .then(data => data.json())
             .then(json => {
                 console.log(json)
+                console.log(json.coord.lon)
+                console.log(json.coord.lat)
+                CurrentWeather(json.coord.lat, json.coord.lon)
             })
             .catch(err => console.log('Erro: ', err))
 }
 
+
+
+function CurrentWeather(lat, lon){
+    fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apikey}`)
+    .then(data => data.json())
+    .then(json => {
+        console.log(json)
+    })
+    .catch(err => console.log('Erro: ', err))
+    console.log('Lat: ',lat)
+    console.log('Lon: ',lon)
+}
